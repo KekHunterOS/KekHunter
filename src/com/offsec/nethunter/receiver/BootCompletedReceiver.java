84e5a107a34fabb5075a155fb2c781952df6fbbd
simonpunk <simonpunk2016@gmail.com>
2019-10-06 02:25:25 +0800

    Added tag name for the class.

--- src/com/offsec/nethunter/receiver/BootCompletedReceiver.java
+++ src/com/offsec/nethunter/receiver/BootCompletedReceiver.java
@@ -3,18 +3,18 @@ package com.offsec.nethunter.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.util.Log;
 
 import com.offsec.nethunter.service.RunAtBootService;
 
 public class BootCompletedReceiver extends BroadcastReceiver{
-
+        private static final String TAG = "BootCompletedReceiver";
         @Override
         public void onReceive(Context context, Intent intent) {
             if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
                 Intent serviceIntent = new Intent(context, RunAtBootService.class);
                 context.startService(serviceIntent);
+                Log.d(TAG, "Nethunter receive boot_completed intent!!");
             }
         }
 }
