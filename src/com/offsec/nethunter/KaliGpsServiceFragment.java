242cfb91b9261dcfb31b08f0f654a079a4f5e318
yesimxev <3924854-yesimxev@users.noreply.gitlab.com>
2020-10-28 06:46:24 +0000

    Revert "Updated Wardriving to use with BlueNMEA"
    This reverts commit aa264ad8091a70a14b96d59bd011f19bfc117b06

--- src/com/offsec/nethunter/KaliGpsServiceFragment.java
+++ src/com/offsec/nethunter/KaliGpsServiceFragment.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -53,10 +52,6 @@ public class KaliGpsServiceFragment extends Fragment implements KaliGPSUpdates.R
                 gpsProvider.onLocationUpdatesRequested(KaliGpsServiceFragment.this);
                 gpsTextView.append("Starting gps updates \n");
             }
-        }, rootView);
-
-        addClickListener(R.id.start_kismet_no_gpsd, v -> {
-            startKismet();
         }, rootView);
 
         addClickListener(R.id.gps_stop, v -> {
